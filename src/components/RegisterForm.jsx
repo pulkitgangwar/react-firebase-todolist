3c30fac5eaf8d48e4401c78f35cda2054cad0aa5
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-11-17 13:39:31 +0530

    added registerform component

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Input from "../components/Input";
+import Button from "../components/Button";
+
+const RegisterForm = ({
+  email,
+  setEmail,
+  password,
+  setPassword,
+  onSubmit,
+  disabled,
+  errors,
+}) => {
+  return (
+    <form className="registerform" onSubmit={onSubmit}>
+      <Input
+        value={email}
+        placeholder="Enter your email"
+        onInputChange={setEmail}
+        label="Email"
+        type="text"
+        error={errors.email}
+      />
+      <Input
+        value={password}
+        placeholder="Enter your password"
+        onInputChange={setPassword}
+        label="Password"
+        type="password"
+        error={errors.password}
+      />
+      <Button buttonType="submit">Register</Button>
+    </form>
+  );
+};
+
+RegisterForm.propTypes = {
+  email: PropTypes.string.isRequired,
+  setEmail: PropTypes.func.isRequired,
+  password: PropTypes.string.isRequired,
+  setPassword: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  errors: PropTypes.shape({
+    email: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired,
+  }),
+};
+
+export default RegisterForm;
