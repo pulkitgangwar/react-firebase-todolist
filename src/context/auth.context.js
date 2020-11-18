6e75d9073dd081af852bd89d8de82c9613dc1eeb
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-11-18 21:12:55 +0530

    added basic auth functions

---
+++ src/context/auth.context.js
@@ -0,0 +1,56 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loadingUser, setLoadingUser] = useState(true);
+
+  const login = ({ email, password }) => {
+    return auth.signInWithEmailAndPassword(email, password);
+  };
+
+  const register = ({ email, password }) => {
+    return auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  const logOut = () => {
+    return auth.signOut();
+  };
+
+  const forgotPassword = ({ email }) => {
+    return auth.sendPasswordResetEmail(email);
+  };
+
+  useEffect(() => {
+    auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+
+  useEffect(() => {
+    setLoadingUser(false);
+  }, [user]);
+
+  const contextObj = {
+    user,
+    login,
+    loadingUser,
+    logOut,
+    register,
+    forgotPassword,
+  };
+
+  return (
+    <AuthContext.Provider value={contextObj}>{children}</AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const auth = useContext(AuthContext);
+  if (!auth) {
+    throw new Error("Context should be used inside provider");
+  }
+  return auth;
+};
