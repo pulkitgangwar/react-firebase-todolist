bb00079058405566b17dac098a6b97079260b43a
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-11-18 21:12:43 +0530

    added authprovider in index.js

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { render } from "react-dom";
 import App from "./App";
 import "./styles/main.scss";
+import { AuthProvider } from "./context/auth.context";
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <AuthProvider>
+    <Router>
+      <App />
+    </Router>
+  </AuthProvider>,
   document.getElementById("root")
 );
