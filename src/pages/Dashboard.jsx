f9d3eeaabc76428b5b59ad3161c51a7b6f04bf2d
pulkitgangwar <pulkitgangwar2016@gmail.com>
2020-11-19 16:45:11 +0530

    added working logout button in dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useAuth } from "../context/auth.context";
 
 const Dashboard = () => {
-  return <div>This is the protected Dashboard</div>;
+  const { logOut } = useAuth();
+  return (
+    <div>
+      <h1>Protected Dashboard</h1>
+      <button onClick={logOut}>Log Out</button>
+    </div>
+  );
 };
 
 export default Dashboard;
